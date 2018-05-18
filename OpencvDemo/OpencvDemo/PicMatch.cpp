4d414428fbcd5844663b55fe3802a5b7b4934077
sand <sand@cretve.com>
2018-05-18 11:38:27 +0800

    my opecv study note

---
+++ OpencvDemo/OpencvDemo/PicMatch.cpp
@@ -0,0 +1,71 @@
+////
+////  PicMatch.cpp
+////  OpencvDemo
+////
+////  Created by wangzy on 16/4/1.
+////  Copyright © 2016年 wangzy. All rights reserved.
+////
+//
+//
+//#include <iostream>
+//#include <vector>
+//
+//#include <opencv2/core/core.hpp>
+////#include <opencv2/features2d/features2d.hpp>
+//#include <opencv2/highgui/highgui.hpp>
+////#include <opencv2/nonfree/nonfree.hpp>
+////#include <opencv2/nonfree/features2d.hpp>
+////#include <opencv2/legacy/legacy.hpp>
+//
+//
+//
+//using namespace cv;
+//using namespace std;
+//
+////
+//int main(int argc, const char * argv[])
+//
+//{
+//
+//    printf(CV_VERSION);
+////
+//    Mat img=imread("/Users/wangzy/ttt.png");
+////
+//    imshow("Load img",img);
+//
+//    waitKey(6000);
+//
+//
+//}
+//
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+//
+//// Include OpenCV's C++ Interface
+//#include "opencv2/opencv.hpp"
+//
+//// Include the rest of our code!
+////#include "detectObject.h"       // Easily detect faces or eyes (using LBP or Haar Cascades).
+//#include "cartoon.h"            // Cartoonify a photo.
+//#include "ImageUtils.h"      // Shervin's handy OpenCV utility functions.
+//
+//using namespace cv;
+//using namespace std;
+//
+//
+
+//
+//int main(){
+//
+//    printf("end-----");
+//    
+//    return 0;
+//}
+//
+
+
+
+
---
+++ OpencvDemo/OpencvDemo/main.cpp
@@ -0,0 +1,19 @@
+//#include <opencv2/opencv.hpp>
+//
+//using namespace cv;
+//
+
+
+int main(int argc, const char * argv[])
+
+{
+
+    
+//    Mat img=imread("/Users/wangzy/ttt.png");
+//    
+//    imshow("Load img",img);
+//    
+//    waitKey(6000);
+//  
+    
+}
